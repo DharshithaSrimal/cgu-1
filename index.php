50a37ef7a493e7965d5b481ed653c1bb7eb37e04
akalanka345 <akalanka345@gmail.com>
2020-03-28 12:29:00 +0530

    sample index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>Career Guidance Unit - UOK</title>
+</head>
+
+<body>
+    <h1>Welcome</h1><br>
+    <h2>Career Guidance Unit - UOK</h2>
+</body>
+
+</html>
