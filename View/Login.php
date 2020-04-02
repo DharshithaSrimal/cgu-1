e35b9853f4f1374d43a69a5bd7317555395d7a37
Rasitha Gamage <g.rasitha@gmail.com>
2020-04-02 23:00:20 +0530

    basic file strucure setup

---
+++ View/Login.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Career Guidance Unit - UOK</title>
+  <meta name="description" content="Career Guidance Unit - System">
+  <meta name="author" content="CGU-UOK">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <script src="res/js/LoginScript.js"></script>
+</body>
+</html>
+
+<?php
+
+$sdsd = "";
+?>
+
