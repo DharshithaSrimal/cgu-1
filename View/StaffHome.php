fb3951c65d6a9b948e60851e8764e5c6fb0b7cc1
Rasitha Gamage <g.rasitha@gmail.com>
2020-04-21 21:27:12 +0530

    Search and star rating functioning

--- View/StaffHome.php
+++ View/StaffHome.php
@@ -1,5 +1,6 @@
 <?php include_once '../Controller/UserController.php'; loadData();?>
 <link rel="stylesheet" href="../res/css/staffHome.css">
+<script src="../res/js/staffHome.js"></script>
 <div class="container bootstrap snippets">
     <div class="row decor-default">
         <div class="col-lg-3 col-md-4 col-sm-12">
@@ -49,55 +50,54 @@
                         <input type="text" class="contacts-list-search" placeholder="Search......." name="search">
                         <button type="button" class="btn" id="search_button"><i class="fa fa-search"></i></button>
                     </div>
+                    <div class="allUnits">
+                        <?php
 
-                  <?php
+                        include_once '../Controller/HomeController.php';
+                        $stu_ar = loadStudentList();
+
+                        foreach ($stu_ar as $stu) {
+                            $id=str_replace("/","",$stu->getUser_id());
+                            echo "  <div class=\"unit\">
+                                        <div class=\"field name\">
+                                            <div>
+                                                <div class='avatartFrame'> 
+                                                    <img src=\"data:image/jpg;base64,".base64_encode($stu->getImage())."\" alt=\"image\" class=\"avatar\"> 
+                                                </div>
+                                                <div class=\"stuName\">".$stu->getFname()." ".$stu->getLname()."  </div>
+                                                    <fieldset id=\"".$id."\" class=\"rating\">
+                                                        <input type=\"radio\" id=\"".$id."star5\" name=\"".$id."rating\" value=\"5\" /><label class = \"full\" for=\"star5\" title=\"Very Good\"></label>
+                                                        <input type=\"radio\" id=\"".$id."star4half\" name=\"".$id."rating\" value=\"4 and a half\" /><label class=\"half\" for=\"star4half\" title=\"Very Good\"></label>
+                                                        <input type=\"radio\" id=\"".$id."star4\" name=\"".$id."rating\" value=\"4\" /><label class = \"full\" for=\"star4\" title=\"Good\"></label>
+                                                        <input type=\"radio\" id=\"".$id."star3half\" name=\"".$id."rating\" value=\"3 and a half\" /><label class=\"half\" for=\"star3half\" title=\"Good\"></label>
+                                                        <input type=\"radio\" id=\"".$id."star3\" name=\"".$id."rating\" value=\"3\" /><label class = \"full\" for=\"star3\" title=\"Average\"></label>
+                                                        <input type=\"radio\" id=\"".$id."star2half\" name=\"".$id."rating\" value=\"2 and a half\" /><label class=\"half\" for=\"star2half\" title=\"Average\"></label>
+                                                        <input type=\"radio\" id=\"".$id."star2\" name=\"".$id."rating\" value=\"2\" /><label class = \"full\" for=\"star2\" title=\"Poor\"></label>
+                                                        <input type=\"radio\" id=\"".$id."star1half\" name=\"".$id."rating\" value=\"1 and a half\" /><label class=\"half\" for=\"star1half\" title=\"Poor\"></label>
+                                                        <input type=\"radio\" id=\"".$id."star1\" name=\"".$id."rating\" value=\"1\" /><label class = \"full\" for=\"star1\" title=\"Very Poor\"></label>
+                                                        <input type=\"radio\" id=\"".$id."starhalf\" name=\"".$id."rating\" value=\"half\" /><label class=\"half\" for=\"starhalf\" title=\"Very Poor\"></label>
+                                                    </fieldset>
+                                            </div>
+                                        </div>
+                                        <div class=\"field phone\" style=\"width: 60% !important;height: 100px  !important;\">
+                                             <span style='font-weight: bold'>Notes: </span>
+                                             Dear sir, I changed my profile description. Please review my profile
+                                        </div>
+                                    </div>";
+
+                            echo "<script type=\"text/javascript\">
+                              drawStarRating(\"".$id."\",".$stu->getRate().");
+                              </script>"
+                            ;
+                        }
 
 
-                  include_once '../Controller/HomeController.php';
-                  $stu_ar = loadStudentList();
-
-                  foreach ($stu_ar as $stu) {
-                      $id=str_replace("/","",$stu->getUser_id());
-                      echo "  <div class=\"unit\">
-                        <div class=\"field name\">
-                            <div class=\"check\">
-                                <input id=\"cb2\" name=\"cb1\" type=\"checkbox\">
-                                <label for=\"cb2\"></label>
-                                <svg viewBox=\"0 0 100 100\" xmlns=\"http://www.w3.org/2000/svg\"></svg>
-
-                            </div>
-                            <div>
-                                <div class='avatartFrame'> 
-                                    <img src=\"data:image/jpg;base64,".base64_encode($stu->getImage())."\" alt=\"image\" class=\"avatar\"> 
-                                </div>
-                                ".$stu->getFname()." ".$stu->getLname()."
-                               
-                                    <fieldset id=\"".$id."\" class=\"rating\">
-                                        <input type=\"radio\" id=\"".$id."star5\" name=\"".$id."rating\" value=\"5\" /><label class = \"full\" for=\"star5\" title=\"Very Good\"></label>
-                                        <input type=\"radio\" id=\"".$id."star4half\" name=\"".$id."rating\" value=\"4 and a half\" /><label class=\"half\" for=\"star4half\" title=\"Very Good\"></label>
-                                        <input type=\"radio\" id=\"".$id."star4\" name=\"".$id."rating\" value=\"4\" /><label class = \"full\" for=\"star4\" title=\"Good\"></label>
-                                        <input type=\"radio\" id=\"".$id."star3half\" name=\"".$id."rating\" value=\"3 and a half\" /><label class=\"half\" for=\"star3half\" title=\"Good\"></label>
-                                        <input type=\"radio\" id=\"".$id."star3\" name=\"".$id."rating\" value=\"3\" /><label class = \"full\" for=\"star3\" title=\"Average\"></label>
-                                        <input type=\"radio\" id=\"".$id."star2half\" name=\"".$id."rating\" value=\"2 and a half\" /><label class=\"half\" for=\"star2half\" title=\"Average\"></label>
-                                        <input type=\"radio\" id=\"".$id."star2\" name=\"".$id."rating\" value=\"2\" /><label class = \"full\" for=\"star2\" title=\"Poor\"></label>
-                                        <input type=\"radio\" id=\"".$id."star1half\" name=\"".$id."rating\" value=\"1 and a half\" /><label class=\"half\" for=\"star1half\" title=\"Poor\"></label>
-                                        <input type=\"radio\" id=\"".$id."star1\" name=\"".$id."rating\" value=\"1\" /><label class = \"full\" for=\"star1\" title=\"Very Poor\"></label>
-                                        <input type=\"radio\" id=\"".$id."starhalf\" name=\"".$id."rating\" value=\"half\" /><label class=\"half\" for=\"starhalf\" title=\"Very Poor\"></label>
-                                    </fieldset>
-                            </div>
-    <!--                            <div class=\"lab lab-warning\">Friends</div>-->
-                        </div>
-                        <div class=\"field phone\" style=\"width: 60% !important;height: 100px  !important;\">
-                             <span style='font-weight: bold'>Notes: </span>
-                             Dear sir, I changed my profile description. Please review my profile
-                        </div>
-                    </div>";
-                  }
-                  ?>
+                        ?>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
 
-<script src="../res/js/staffHome.js"></script>
+
--- res/js/staffHome.js
+++ res/js/staffHome.js
@@ -18,9 +18,62 @@ $(function(){
 
 })
 
+
+function drawStarRating(elementID,Rating) {
+//#IM2014030star1half
+    starRating = Rating;
+    console.log(Rating);
+    starRating = Rating==0.5?"starhalf":starRating;
+    starRating = Rating==1?"star1":starRating;
+    starRating = Rating==1.5?"star1half":starRating;
+    starRating = Rating==2?"star2":starRating;
+    starRating = Rating==2.5?"star2half":starRating;
+    starRating = Rating==3?"star3":starRating;
+    starRating = Rating==3.5?"star3half":starRating;
+    starRating = Rating==4?"star4":starRating;
+    starRating = Rating==4.5?"star4half":starRating;
+    starRating = Rating==5?"star5":starRating;
+
+    var id = '#'+elementID+""+starRating;
+    console.log(id);
+    $(id).prop( "checked", true );
+}
+
 window.onload = function () {
 
-    $('#IM2014030star1half').prop( "checked", true );
-    $('#IM2014014star3half').prop( "checked", true );
+
+
+    var inputField = document.querySelector('.contacts-list-search');
+
+    var xx = $('.allUnits').children('.unit');
+
+    inputField.addEventListener('input', (event) => {
+        // const result = document.querySelector('.result');
+        // result.textContent = `You like ${event.target.value}`;
+        var searchVal = event.target.value;
+        xx.each(function (index, element) {
+
+            //console.log(element.querySelector('.stuName').innerHTML+"-"+searchVal);
+
+            if((element.querySelector('.stuName').innerHTML).trim()!= searchVal){
+                element.style.visibility  = "hidden";
+                element.style.display  = "none";
+            }
+            else {
+                element.style.visibility  = "visible";
+                element.style.display  = "inline-block";
+
+            }
+
+            if(searchVal == ""){
+                element.style.visibility  = "visible";
+                element.style.display  = "inline-block";
+            }
+        });
+
+
+
+
+    });
 
 }
