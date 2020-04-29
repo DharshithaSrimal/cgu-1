ff997e11b748c7d7b083887a01cdf6be81050b34
DharshithaSrimal <dharshithasrimal@gmail.com>
2020-04-29 19:25:31 +0530

    Degree

--- res/js/LoadDegree.js
+++ res/js/LoadDegree.js
@@ -5,9 +5,11 @@ $("#faculty").change(function(){ /* WHEN YOU CHANGE AND SELECT FROM THE SELECT F
     $.ajax({ /* THEN THE AJAX CALL */
       type: "POST", /* TYPE OF METHOD TO USE TO PASS THE DATA */
       url: "../Controller/DegreeController.php", /* PAGE WHERE WE WILL PASS THE DATA */
+      dataType: "json",
       data: 'faculty='+faculty+'&method='+method, /* THE DATA WE WILL BE PASSING */
-      success: function(result){ /* GET THE TO BE RETURNED DATA */
+      success: function(object){ /* GET THE TO BE RETURNED DATA */
         //$("#show").html(result); /* THE RETURNED DATA WILL BE SHOWN IN THIS DIV */
+        //var newObj = JSON.parse((object) );
         
       }
     });
