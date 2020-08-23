16c6187486351aba07280bbf7b59debf77a4b5b4
akalanka345 <akalanka345@gmail.com>
2020-08-23 18:22:30 +0530

    admin job posting

---
+++ res/js/Job.js
@@ -0,0 +1,158 @@
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+  }
+  
+  /*An array containing all the country names in the world:*/
+  var countries = ["IT","Management","SE","MIT","Computer Science","AI","Big Data", "Data Science"];
+  
+  /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+  autocomplete(document.getElementById("myInput"), countries);
+
+
+  $("#publishNews").click(function () {
+    var inqType = "ANY";
+    var msg_body = $('#composeNews').summernote('code');
+    msg_body =  msg_body.replace(/\+/g,'%2B');
+    var receiver = $("#receiverList").val();
+    var method = "inqSubmit";
+
+    $.ajax({
+        type: "POST",
+        url: "../Controller/InquiryController.php",
+        data: 'inqType='+inqType+'&msg_body='+msg_body+'&method='+method+'&receiver='+receiver,
+        cache: false,
+        success: function(result){
+
+            if(result == "inq success"){
+                //alert("Successfully Send");
+
+            }
+            if(result == "inq failed"){
+                //alert("Error... Please try again...!!");
+            }
+        }
+    });
+
+    // location.reload();
+
+    setTimeout(window.location.href = "http://localhost:8080/cgu/View/Inquiry.php?newMsg="+receiver.split("/").join('-'),2000)
+});
+
+$("#Searchmatching").click(function(){
+    var tags = $("#tags").val();
+    alert(tags)
+
+    var dataString = {};
+    dataString.tags = tags;
+    dataString.method = "searchStudent";
+
+    $.ajax({
+        type: "POST",
+        url: "../Controller/JobPostingController.php",
+        data: dataString,
+        cache: false,
+        success: function(result){
+            console.log(result);
+            var x = JSON.parse(result);
+            console.log(x);
+            var $output = $('#matchingStudents');
+            for(var i = 0; i < x.length; i++) {
+                $output.append('<div id="'+x[i].user_id+'"></div>');
+                $('#'+x[i].user_id+'').append('<label>Student ID :</label>' + '<input type="text" readonly="readonly" id="textboxId" value="'+x[i].user_id+'" ><br>');
+            }
+        }
+    });
+})
